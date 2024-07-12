2d8cec39faa8ad2bcee9cf50f9324eaa5848f8d2
Renzo2022 <renzofideldiazsotelo@gmail.com>
2024-07-11 19:40:42 -0500

    Agregando boton send para enviar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  form = new FormGroup ({
+    promt: new FormControl('')
+  })
 
   constructor() {}
 
