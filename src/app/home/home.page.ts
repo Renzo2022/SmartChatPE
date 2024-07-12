3110f8da6a4ba052e52922074497aff57b995357
Renzo2022 <renzofideldiazsotelo@gmail.com>
2024-07-11 21:10:12 -0500

    Agregando el chat

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Message } from '../models/message.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,11 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  messages: Message[] = [
+    {sender: 'me', content: 'Hola como estas?'},
+    {sender: 'bot', content: 'Bien y tu, como estas?'},
+  ];
 
   form = new FormGroup ({
     promt: new FormControl('')
